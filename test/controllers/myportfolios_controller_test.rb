ef2535b86f2e56931423001a9c670378be75fbe5
root <keikei1166guiyou@gmail.com>
2021-02-20 06:30:18 +0000

    not minitest

--- test/controllers/myportfolios_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class MyportfoliosControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get myportfolios_index_url
-    assert_response :success
-  end
-
-end
